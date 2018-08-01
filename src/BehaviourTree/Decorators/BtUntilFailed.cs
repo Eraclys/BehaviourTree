4ed4e808bf5df7e0dfc7c14272494ba22e1ac7f9
Milian Lichere <milian.lichere@tekgent.com>
2018-08-01 09:08:00 +0100

    adding UntilFailed and UntilSuccess decorators

---
+++ src/BehaviourTree/Decorators/BtUntilFailed.cs
@@ -0,0 +1,21 @@
+﻿namespace BehaviourTree.Decorators
+{
+    public sealed class BtUntilFailed : BaseBtDecorator
+    {
+        public BtUntilFailed(IBtBehaviour child) : base(child)
+        {
+        }
+
+        protected override BehaviourStatus DoTick(BtContext context)
+        {
+            var childStatus = Child.Tick(context);
+
+            if (childStatus == BehaviourStatus.Failed)
+            {
+                return BehaviourStatus.Succeeded;
+            }
+
+            return BehaviourStatus.Running;
+        }
+    }
+}
---
+++ src/BehaviourTree/Decorators/BtUntilSuccess.cs
@@ -0,0 +1,21 @@
+﻿namespace BehaviourTree.Decorators
+{
+    public sealed class BtUntilSuccess : BaseBtDecorator
+    {
+        public BtUntilSuccess(IBtBehaviour child) : base(child)
+        {
+        }
+
+        protected override BehaviourStatus DoTick(BtContext context)
+        {
+            var childStatus = Child.Tick(context);
+
+            if (childStatus == BehaviourStatus.Succeeded)
+            {
+                return BehaviourStatus.Succeeded;
+            }
+
+            return BehaviourStatus.Running;
+        }
+    }
+}
