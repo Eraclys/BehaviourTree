28a1c51957d82e66318db75fe99360c684c60f73
Milian Lichere <milian.lichere@tekgent.com>
2018-08-02 22:06:05 +0100

    re-implementing auto-reset

---
+++ src/BehaviourTree.Tests/AutoResetTests.cs
@@ -0,0 +1,49 @@
+﻿using BehaviourTree.Decorators;
+using BehaviourTree.Tests.Utils;
+using NUnit.Framework;
+
+namespace BehaviourTree.Tests
+{
+    [TestFixture]
+    internal sealed class AutoResetTests
+    {
+        [TestCase(BehaviourStatus.Failed)]
+        [TestCase(BehaviourStatus.Succeeded)]
+        public void WhenChildTerminates_ResetChild(BehaviourStatus status)
+        {
+            var child = new MockBehaviour { ReturnStatus = status };
+
+            var sut = new AutoReset(child);
+
+            sut.Tick(new BtContext());
+
+            Assert.That(child.ResetCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void WhenChildIsRunning_ContinueRunning()
+        {
+            var child = new MockBehaviour { ReturnStatus = BehaviourStatus.Running };
+
+            var sut = new AutoReset(child);
+
+            sut.Tick(new BtContext());
+
+            Assert.That(child.ResetCount, Is.EqualTo(0));
+        }
+
+        [TestCase(BehaviourStatus.Succeeded)]
+        [TestCase(BehaviourStatus.Running)]
+        [TestCase(BehaviourStatus.Failed)]
+        public void OnTick_ShouldReturnChildStatus(BehaviourStatus status)
+        {
+            var child = new MockBehaviour { ReturnStatus = status };
+
+            var sut = new AutoReset(child);
+
+            var behaviourStatus = sut.Tick(new BtContext());
+
+            Assert.That(behaviourStatus, Is.EqualTo(status));
+        }
+    }
+}
---
+++ src/BehaviourTree/Decorators/AutoReset.cs
@@ -0,0 +1,19 @@
+﻿namespace BehaviourTree.Decorators
+{
+    public sealed class AutoReset : DecoratorBehaviour
+    {
+        public AutoReset(IBehaviour child) : base(child)
+        {
+        }
+
+        protected override BehaviourStatus Update(BtContext context)
+        {
+            return Child.Tick(context);
+        }
+
+        protected override void OnTerminate(BehaviourStatus status)
+        {
+            Child.Reset();
+        }
+    }
+}
